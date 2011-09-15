d3d2fa8d800a1185ba39553d8ef2d9dd43263746
Nick <nal7301@sru.edu>
2011-09-14 22:08:04 -0400

    nothing

--- src/edu/sru/distributedprocessing/VehicleTabWidget.java
+++ src/edu/sru/distributedprocessing/VehicleTabWidget.java
@@ -4,9 +4,15 @@ import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TabHost;
 
 public class VehicleTabWidget extends TabActivity{
+	private Button vehicles_btn, drivers_btn, shipments_btn, routing_btn, contractor_btn,
+	depot_btn, warehouse_btn, vehicle_type_btn, maintenance_btn, tech_btn, contact_btn, report_btn;
+
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.vehicles_main);
@@ -44,5 +50,104 @@ public class VehicleTabWidget extends TabActivity{
 	    tabHost.addTab(spec);
 	   
 	    tabHost.setCurrentTab(0);
+	    
+	    vehicles_btn = (Button) findViewById(R.id.vehicle_btn);
+        vehicles_btn.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				Intent engineIntent = new Intent(VehicleTabWidget.this, VehicleTabWidget.class);
+				startActivity(engineIntent);
+			}
+		});
+        
+        drivers_btn = (Button) findViewById(R.id.drivers_btn);
+        drivers_btn.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+			
+			}
+        });
+        
+        shipments_btn = (Button) findViewById(R.id.shipments_btn);
+        shipments_btn.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+			
+			}
+        });
+        
+        routing_btn = (Button) findViewById(R.id.routing_btn);
+        routing_btn.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				
+			}
+        });
+        
+        contractor_btn = (Button) findViewById(R.id.contractors_btn);
+        contractor_btn.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				
+			}
+        });
+        
+        depot_btn = (Button) findViewById(R.id.depots_btn);
+        depot_btn.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				
+			}
+        });
+        
+        warehouse_btn = (Button) findViewById(R.id.warehouses_btn);
+        warehouse_btn.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				
+			}
+        });
+        
+        vehicle_type_btn = (Button) findViewById(R.id.vehicle_type_btn);
+        vehicle_type_btn.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				
+			}
+        });
+        
+        maintenance_btn = (Button) findViewById(R.id.maintenance_btn);
+        maintenance_btn.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				
+			}
+        });
+        
+        tech_btn = (Button) findViewById(R.id.technicians_btn);
+        tech_btn.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				
+			}
+        });
+        
+        contact_btn = (Button) findViewById(R.id.contacts_btn);
+        contact_btn.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				
+			}
+        });
+        
+        report_btn = (Button) findViewById(R.id.reports_btn);
+        report_btn.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				
+			}
+        });
 	}
 }

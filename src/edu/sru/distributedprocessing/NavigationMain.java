15915cec53f71b96ada3de7180238be792fac77a
Nick <nal7301@sru.edu>
2011-09-30 12:18:50 -0400

    new main

--- src/edu/sru/distributedprocessing/NavigationMain.java
+++ src/edu/sru/distributedprocessing/NavigationMain.java
@@ -1,16 +1,13 @@
 package edu.sru.distributedprocessing;
 
-import edu.sru.distributedprocessing.editors.VehicleEditor;
-import edu.sru.distributedprocessing.optionslist.Options;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,144 +23,51 @@ public class NavigationMain extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.navigation_main);
-                
-        header_txt = (TextView) findViewById(R.id.header_txt1);
-        header_txt.setText("Welcome to the Navigation Menu");
-        
-        vehicle_btn = (Button) findViewById(R.id.vehicle_btn);
-        vehicle_btn.setOnClickListener(new OnClickListener()
-        {
-			//@Override
-			public void onClick(View v) 
-			{
-				finish();
-				Intent engineIntent = new Intent(NavigationMain.this, IntelliSyncActivity.class);
-				engineIntent.putExtra("Type", "VehicleType");
-				startActivity(engineIntent);
-			}
-		});
-        
-        drivers_btn = (Button) findViewById(R.id.drivers_btn);
-        drivers_btn.setOnClickListener(new OnClickListener()
-        {
-			//@Override
-			public void onClick(View v) 
-			{
-				finish();
-				Intent engineIntent = new Intent(NavigationMain.this, IntelliSyncActivity.class);
-				engineIntent.putExtra("Type", "DriverType");
-				startActivity(engineIntent);
-			}
+              
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+            	Intent engineIntent = new Intent(NavigationMain.this, IntelliSyncActivity.class);
+
+            	if(position == 0)
+            	{
+            		//driver
+            		engineIntent.putExtra("Type", "VehicleType");
+            		startActivity(engineIntent);
+            	}else
+            		if(position == 1)
+            		{
+            			//contact
+            			engineIntent.putExtra("Type", "ContactType");
+            			startActivity(engineIntent);
+            		}else
+            			if(position == 2)
+            			{
+            				//driver
+            				engineIntent.putExtra("Type","DriverType");
+            				startActivity(engineIntent);
+            			}else
+                			if(position == 3)
+                			{
+                				//vehicle type
+                				engineIntent.putExtra("Type", "VehicleTypesType");
+                				startActivity(engineIntent);
+                			}else
+                    			if(position == 4)
+                    			{
+                    				//warehouse
+                    			}else
+                        			if(position == 5)
+                        			{
+                        				//depot
+                        				engineIntent.putExtra("Type", "DepotType");
+                        				startActivity(engineIntent);  
+                        			}
+            }
         });
-        
-        shipments_btn = (Button) findViewById(R.id.shipments_btn);
-        shipments_btn.setOnClickListener(new OnClickListener()
-        {
-			//@Override
-			public void onClick(View v) 
-			{
-				Toast.makeText(NavigationMain.this, "Shipments Button Selected", Toast.LENGTH_SHORT).show();
-			}
-        });
-        
-        routing_btn = (Button) findViewById(R.id.routing_btn);
-        routing_btn.setOnClickListener(new OnClickListener()
-        {
-			//@Override
-			public void onClick(View v) 
-			{
-				Toast.makeText(NavigationMain.this, "Routing Button Selected", Toast.LENGTH_SHORT).show();
-			}
-        });
-        
-        contractors_btn = (Button) findViewById(R.id.contractors_btn);
-        contractors_btn.setOnClickListener(new OnClickListener()
-        {
-			//@Override
-			public void onClick(View v) 
-			{
-				Toast.makeText(NavigationMain.this, "Contractors Button Selected", Toast.LENGTH_SHORT).show();
-			}
-        });
-        
-        depots_btn = (Button) findViewById(R.id.depots_btn);
-        depots_btn.setOnClickListener(new OnClickListener()
-        {
-			//@Override
-			public void onClick(View v) 
-			{
-				finish();
-				Intent engineIntent = new Intent(NavigationMain.this, IntelliSyncActivity.class);
-				engineIntent.putExtra("Type", "DepotType");
-				startActivity(engineIntent);
-			}
-        });
-        
-        warehouses_btn = (Button) findViewById(R.id.warehouses_btn);
-        warehouses_btn.setOnClickListener(new OnClickListener()
-        {
-			//@Override
-			public void onClick(View v) 
-			{
-				Toast.makeText(NavigationMain.this, "Warehouses Button Selected", Toast.LENGTH_SHORT).show();
-			}
-        });
-        
-        vehicle_type_btn = (Button) findViewById(R.id.vehicle_type_btn);
-        vehicle_type_btn.setOnClickListener(new OnClickListener()
-        {
-			//@Override
-			public void onClick(View v) 
-			{
-				finish();
-				Intent engineIntent = new Intent(NavigationMain.this, IntelliSyncActivity.class);
-				engineIntent.putExtra("Type", "VehicleTypesType");
-				startActivity(engineIntent);
-			}
-        });
-        
-        maintenance_btn = (Button) findViewById(R.id.maintenance_btn);
-        maintenance_btn.setOnClickListener(new OnClickListener()
-        {
-			//@Override
-			public void onClick(View v) 
-			{
-				Toast.makeText(NavigationMain.this, "Maintenance Button Selected", Toast.LENGTH_SHORT).show();
-			}
-        });
-        
-        technicians_btn = (Button) findViewById(R.id.technicians_btn);
-        technicians_btn.setOnClickListener(new OnClickListener()
-        {
-			//@Override
-			public void onClick(View v) 
-			{
-				Toast.makeText(NavigationMain.this, "Technicians Button Selected", Toast.LENGTH_SHORT).show();
-			}
-        });
-        
-        contacts_btn = (Button) findViewById(R.id.contacts_btn);
-        contacts_btn.setOnClickListener(new OnClickListener()
-        {
-			//@Override
-			public void onClick(View v) 
-			{
-				finish();
-				Intent engineIntent = new Intent(NavigationMain.this, IntelliSyncActivity.class);
-				engineIntent.putExtra("Type", "ContactType");
-				startActivity(engineIntent);
-			}
-        });
-        
-        reports_btn = (Button) findViewById(R.id.reports_btn);
-        reports_btn.setOnClickListener(new OnClickListener()
-        {
-			//@Override
-			public void onClick(View v) 
-			{
-				Toast.makeText(NavigationMain.this, "Reports Button Selected", Toast.LENGTH_SHORT).show();
-			}
-        });
+       
     }
     
 }
